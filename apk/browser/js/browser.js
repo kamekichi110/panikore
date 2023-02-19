6dae9169a1b65e5063cd71f3385aa30701e4bb4f
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-02-20 07:38:01 +0900

    ui v1

---
+++ apk/browser/js/browser.js
@@ -0,0 +1,8 @@
+function url_go() {
+    var url = document.getElementById('url');
+    browser.location = url;
+}
+function home() {
+    var url = document.getElementById('url');
+    browser.location = ;
+}
